a44aacce0cc5f16e8890ab039e328ee63f40ae19
lbzinuse75 <lbzinuse@hotmail.com>
2023-09-23 22:20:56 -0600

    Fix W2

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -1,7 +1,6 @@
 /* W02-Task - Profile Home Page */
 
 /* Step 1 - Setup type tasks - no code required */
-console.log("My name is Becky");
 
 /* Step 2 - Variables */
 let fullName = "Becky Lutz";
@@ -13,7 +12,7 @@ let profilePicture = "images/IMG_6989_Original.jpg";
 const nameElement = document.getElementById("name");
 const foodElement = document.getElementById("food");
 const yearElement = document.querySelector("#year");
-const imageElement = document.querySelector("#image");
+const imageElement = document.querySelector("img");
 
 
 /* Step 4 - Adding Content */
