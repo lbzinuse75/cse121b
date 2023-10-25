dbd77b4a6911d4cbbd71cce8c9262ef870acce4c
lbzinuse75 <lbzinuse@hotmail.com>
2023-10-24 20:13:52 -0600

    Final complete

--- final/scripts/project.js
+++ final/scripts/project.js
@@ -4,6 +4,9 @@ const welcome = document.querySelector("#welcome");
 welcome.innerHTML = "Welcome to the random Pokemon generator";
 
 const button = document.getElementById("button");
+const arrayOutput = document.querySelector("#array");
+
+const pokemonArray = [];
 
 button.addEventListener("click", () => {
     const random = Math.floor(Math.random() * 1017) + 1;
@@ -16,16 +19,17 @@ async function getPokemon(apiUrl) {
         const response = await fetch (apiUrl);
         if (response.ok) {
             const data = await response.json();
-            doStuff(data);
+            showPokemon(data);
+            storeInArray(data);
         } else {
             console.error("Failed to fetch data from the API.");
         }
     } catch (error) {
-        console.error("An error occurred while fetching Pokemon datat:", error);
+        console.error("An error occurred while fetching Pokemon data:", error);
     }
 }
 
-function doStuff(data) {
+function showPokemon(data) {
     const output = document.querySelector("#info");
 
     results = data;
@@ -37,6 +41,31 @@ function doStuff(data) {
     document.querySelector("#id").textContent = `ID: ${results.id}`;
     document.querySelector("#weight").textContent = `Weight: ${results.weight}`;
     document.querySelector("#height").textContent = `Height: ${results.height}`;
+}
+
+
+
+function storeInArray(data) {
+    const pokemonArrayData = {
+        name: results.name,
+        id: results.id,
+        weight: results.weight,
+        height: results.height,
+    };
+
+    pokemonArray.push(pokemonArrayData);
+    pokemonArray.sort((a, b) => a.weight - b.weight);
+    displayPokemonArray(pokemonArray);
+}
+
+function displayPokemonArray(pokemonArray) {
+    arrayOutput.innerHTML = "";
+
+    pokemonArray.forEach((pokemon, index) => {
+        const pokemonArrayInfo = document.createElement("div");
+        pokemonArrayInfo.textContent = `${index + 1}. Name: ${pokemon.name}, ID: ${pokemon.id}, Weight: ${pokemon.weight}, Height: ${pokemon.height}`;
+        arrayOutput.appendChild(pokemonArrayInfo);
+    });
 }
   
 getPokemon("https://pokeapi.co/api/v2/pokemon/1/");
